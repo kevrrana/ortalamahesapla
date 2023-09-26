37a9f27de040519dccb65b390037811ddca6aaf4
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-09-26 15:31:23 +0300

    Add files via upload

---
+++ ortalamaornek/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ortalamaornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSinav1 = new System.Windows.Forms.Label();
+            this.lblSinav2 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtSinav1 = new System.Windows.Forms.TextBox();
+            this.txtSinav2 = new System.Windows.Forms.TextBox();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSinav1
+            // 
+            this.lblSinav1.AutoSize = true;
+            this.lblSinav1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinav1.Location = new System.Drawing.Point(85, 72);
+            this.lblSinav1.Name = "lblSinav1";
+            this.lblSinav1.Size = new System.Drawing.Size(78, 20);
+            this.lblSinav1.TabIndex = 1;
+            this.lblSinav1.Text = "Sınav 1 :";
+            this.lblSinav1.Click += new System.EventHandler(this.lblSinav1_Click);
+            // 
+            // lblSinav2
+            // 
+            this.lblSinav2.AutoSize = true;
+            this.lblSinav2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSinav2.Location = new System.Drawing.Point(85, 117);
+            this.lblSinav2.Name = "lblSinav2";
+            this.lblSinav2.Size = new System.Drawing.Size(78, 20);
+            this.lblSinav2.TabIndex = 2;
+            this.lblSinav2.Text = "Sınav 2 :";
+            this.lblSinav2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnHesapla.Location = new System.Drawing.Point(145, 177);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(85, 55);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtSinav1
+            // 
+            this.txtSinav1.BackColor = System.Drawing.Color.Tan;
+            this.txtSinav1.Location = new System.Drawing.Point(203, 72);
+            this.txtSinav1.Multiline = true;
+            this.txtSinav1.Name = "txtSinav1";
+            this.txtSinav1.Size = new System.Drawing.Size(132, 20);
+            this.txtSinav1.TabIndex = 5;
+            this.txtSinav1.TextChanged += new System.EventHandler(this.txtSınav1_TextChanged);
+            // 
+            // txtSinav2
+            // 
+            this.txtSinav2.BackColor = System.Drawing.Color.Tan;
+            this.txtSinav2.Location = new System.Drawing.Point(203, 117);
+            this.txtSinav2.Name = "txtSinav2";
+            this.txtSinav2.Size = new System.Drawing.Size(132, 20);
+            this.txtSinav2.TabIndex = 6;
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.BackColor = System.Drawing.Color.Tan;
+            this.txtOrtalama.Enabled = false;
+            this.txtOrtalama.Location = new System.Drawing.Point(203, 252);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(132, 20);
+            this.txtOrtalama.TabIndex = 8;
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrtalama.Location = new System.Drawing.Point(85, 252);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(92, 20);
+            this.lblOrtalama.TabIndex = 7;
+            this.lblOrtalama.Text = "Ortalama :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.txtSinav2);
+            this.Controls.Add(this.txtSinav1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblSinav2);
+            this.Controls.Add(this.lblSinav1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSinav1;
+        private System.Windows.Forms.Label lblSinav2;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtSinav1;
+        private System.Windows.Forms.TextBox txtSinav2;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Label lblOrtalama;
+    }
+}
+
---
+++ ortalamaornek/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ortalamaornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSinav1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSınav1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            float sinav1, sinav2, ortalama;
+            sinav1 = Convert.ToSingle(txtSinav1.Text);
+            sinav2 = Convert.ToSingle(txtSinav2.Text);
+            ortalama=(sinav1+sinav2)/2;
+
+            txtOrtalama.Text=ortalama.ToString();
+        }
+    }
+}
+
+        
+        
+
+
+
+
+
+
+
+
+        
